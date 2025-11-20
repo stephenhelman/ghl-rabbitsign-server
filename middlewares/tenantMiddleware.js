0a79a38829b6a285a02980f1cf97e7f59f4b46ed
Stephen Helman <stephenhelman18@gmail.com>
2025-11-20 09:58:38 -0700

    Middlewares set up

--- middlewares/tenantMiddleware.js
+++ middlewares/tenantMiddleware.js
@@ -1,1 +1,32 @@
 //resolve tenant from :tenantId param
+const tenantService = require("../services/tenantService");
+const tenantMiddleware = async (req, res, next) => {
+  try {
+    const tenantId = req.params.tenantId;
+
+    if (!tenantId) {
+      return res.status(400).json({
+        ok: false,
+        error: "Missing tenantId in route parameters",
+      });
+    }
+
+    const tenant = await tenantService.getTenantById(tenantId);
+
+    if (!tenant) {
+      return res.status(404).json({
+        ok: false,
+        error: `Tenant not found for id: ${tenantId}`,
+      });
+    }
+
+    req.tenant = tenant;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+};
+
+module.exports = {
+  tenantMiddleware,
+};
